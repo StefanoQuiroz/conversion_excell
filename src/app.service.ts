7fe9b854a0165ed209fb8e72c3212c7f406d887d
Stefano <piero.quiroz93@gmail.com>
2023-08-30 21:06:35 -0500

    Init_1

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export class AppService {
   async getCommits() {
     const url =
-      'https://api.github.com/repos/JoacoMaurtua/Nebulabs-img-generator/commits';
+      'https://api.github.com/repos/JoacoMaurtua/Commit-Logger/commits';
 
     const response = await axios.get(url);
     return response.data;
